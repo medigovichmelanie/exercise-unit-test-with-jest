e49c9f8bd26fb97a9ddedd48d975e9b34b673047
Melanie Dana Medigovich <melacem@gmail.com>
2025-04-21 21:27:22 +0000

    exercise fixed

--- test.js
+++ test.js
@@ -1,32 +1,19 @@
+const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
+
 test("One euro should be 1.07 dollars", function() {
-    
-    const { fromEuroToDollar } = require('./app.js');
-
     const dollars = fromEuroToDollar(3.5);
-
     const expected = 3.5 * 1.07;
-
     expect(dollars).toBe(expected);
-})
+});
 
 test("One dollar should be 156.5 yenes", function() {
-
-    const { fromDollarToYen } = require('./app.js');
-
     const yen = fromDollarToYen(6);
-
     const expected = 6 * 156.5;
-
     expect(yen).toBe(expected);
-})
+});
 
 test("One yen should be 0.87 pounds", function() {
-
-    const { fromYenToPound } = require('./app.js');
-
     const pound = fromYenToPound(8);
-
     const expected = 8 * 0.87;
-
     expect(pound).toBe(expected);
-})
+});
